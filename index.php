8cca947faa6a8ea6faf170aaf82d032e0a4f70c3
Justin <ju3tin@hotmail.co.uk>
2017-04-12 14:31:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
    <link type="text/css" rel="stylesheet" href="login.css">
   <title>Google Maps Multiple Markers</title>
   <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
+   
+  <script src="index.js" type="text/javascript"></script>
 </head>
 <body>
 
